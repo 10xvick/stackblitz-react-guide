06def86be9575d51c69f9bb63e17d742d4e91716
10xvick <103027108+10xvick@users.noreply.github.com>
2023-09-06 13:54:24 +0530

    2 files modified

--- examples/Ex_Suspense.tsx
+++ examples/Ex_Suspense.tsx
@@ -1,20 +1,17 @@
-import { Suspense, useEffect, useState } from "react";
+import { lazy, Suspense, useEffect, useState } from "react";
 
 export default function Ex_Suspense(){
-  const [Data,setData] = useState(null);
+  return <App/>
+}
 
-  useEffect(()=>{
-    const ref = setInterval(()=>{
-      setData(e=>(e?undefined:'x'));
-    },2000);
+const image = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fcommons.wikimedia.org%2Fwiki%2FFile%3APizigani_1367_Chart_10MB.jpg&psig=AOvVaw0ew0E8N1mnNPSn3rLzVy1H&ust=1694074881881000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCKC_vPHGlYEDFQAAAAAdAAAAABAE'
 
-    return ()=>{
-      clearInterval(ref);
-    }
-  }, [])
-  return <div>
-    <Suspense fallback={<div>Loading...</div>}>
-      {Data}
-    </Suspense>
-  </div>
+function App() {
+  return (
+    <div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <img style={{width:'100%'}} src={image}/>
+      </Suspense>
+    </div>
+  );
 }
