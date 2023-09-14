b6e366d38f503ba7ae0953793ed268c2d4378dc0
10xvick <103027108+10xvick@users.noreply.github.com>
2023-09-14 17:18:28 +0530

    2 files modified

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,35 +1,20 @@
 import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import { components, routes } from './Ex_container';
+import { routes } from './Ex_container';
 
 export default function Sidebar({ select, selected }) {
   const [type, settype] = useState(true);
 
   return (
     <div className="sidebar">
-      <span
-        onClick={() => settype(!type)}
-        className="sidebar-text sidebar-list"
-      >
-        III
-      </span>
-
       <div className="sidebar-list">
         {routes.map((e) => (
-          <NavLink to={e.name}>{e.name}</NavLink>
+          <NavLink to={e.name} >{e.name}</NavLink>
         ))}
+        <a className="sidebar-text sidebar-list">III</a>
       </div>
     </div>
   );
 }
 
-function Item({ name, select, selected }) {
-  return (
-    <span
-      onClick={() => select(name)}
-      className={`sidebar-text ${name == selected ? 'active' : ''}`}
-    >
-      {name}
-    </span>
-  );
-}
+
