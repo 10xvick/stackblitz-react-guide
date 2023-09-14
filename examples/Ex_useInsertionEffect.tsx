3e1c60e757d5039ef87389099575c89a2e86f991
10xvick <103027108+10xvick@users.noreply.github.com>
2023-09-14 18:31:57 +0530

    1 file modified

--- examples/Ex_useInsertionEffect.tsx
+++ examples/Ex_useInsertionEffect.tsx
@@ -1,37 +1,33 @@
-import { useEffect, useInsertionEffect, useLayoutEffect, useRef, useState } from "react"
+import { useInsertionEffect, useRef, useState } from 'react';
 
-export default function Ex_useInsertionEffect(){
-  const [count,setcount] = useState(0);
+export default function Ex_useInsertionEffect() {
+  const [count, setcount] = useState(0);
   const ref = useRef();
 
-  let x = 0;
-  function test(){
-    console.log( document.getElementById('btn')?.innerText,  ref.current?.innerText, ++x);
-  }
+  useInsertionEffect(() => {
+    const e = document.getElementById('btn') || ref.current?.innerText;
+    console.log(
+      e ||
+        'element is not found on in the dom since it has not been inserted yet'
+    );
+    if (e) e.style.color = 'yellow';
+  }, []);
 
-  const state = {};
-  useEffect(()=>{
-    test();
-    state['useEffect'] = 0;
-  });
-  
-  useLayoutEffect(()=>{
-    test();
-    state['useLayoutEffect'] = 0;
-  });
-  
-  useInsertionEffect(()=>{
-    test();
-    state['useInsertionEffect'] = 0;
-  }); 
-
-  return <button id='btn' ref={ref} onClick={()=>{setcount(count+1)}}>{count}</button>
+  return (
+    <button
+      id="btn"
+      ref={ref}
+      onClick={() => {
+        setcount(count + 1);
+      }}
+    >
+      rerender
+    </button>
+  );
 }
-
 
 /**
  
   useInsertionEffect is called "Insertion" because it runs synchronously before all other DOM mutations have been applied. This allows you to perform side effects that need to run before any other styles or elements are inserted into the DOM, such as inserting global DOM nodes like 'style' or SVG 'defs' in the document head.
 
  */
-
