c89e2183c7086f0e56eac6173487ff9ed4d1ee8c
10xvick <103027108+10xvick@users.noreply.github.com>
2024-05-11 18:17:31 +0530

    2 files modified

--- src/decorators/Decorator_HOC.tsx
+++ src/decorators/Decorator_HOC.tsx
@@ -1,13 +1,20 @@
-import { defenitions } from "../data/defenitions";
+import { defenitions } from '../data/defenitions';
 
-
-export default function Decorator_HOC(Component,name){
-  return (props)=><div className='container'>
-    <div>{defenitions[name]}</div>
-    <br/>
-    <p>Example:</p>
-    <div className='component-panel'>
-      <Component {...props}/>
+export default function Decorator_HOC(Component, name) {
+  return (props) => (
+    <div className="container">
+      <div>{defenitions[name]}</div>
+      <br />
+      <p>Example:</p>
+      <div className="component-panel">
+        <Component {...props} />
+      </div>
+      <div>
+        <iframe
+          className="sbwebcontainer"
+          src={`https://stackblitz.com/edit/stack-react-guide?embed=1&file=examples%2FEx_${name}.tsx&hideExplorer=1&hideNavigation=1&view=editor&hidedevtools=1`}
+        ></iframe>
+      </div>
     </div>
-  </div>
+  );
 }
